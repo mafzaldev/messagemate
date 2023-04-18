04da289ae85ac061eda687779c765a8cde19411f
Afzal <afzalashraf814@gmail.com>
2023-04-18 11:49:27 +0500

    :books:changed app name in different locations

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-white">Welcome to Chatter</h1>
+      <h1 className="text-3xl font-bold text-white">Welcome to MessageMate</h1>
       <div className="text-md text-white mt-1 mb-5">
         This is the beginning of the chat
       </div>
